d166142d8e963cf18a10e0b3c7f5347ffafe1b54
Isabela Müller <isabelamuller1@hotmail.com>
2026-09-25 18:39:52 -0300

    fix bottom menu mobile

--- components/BottomMenu/index.tsx
+++ components/BottomMenu/index.tsx
@@ -3,7 +3,7 @@ import { menuItems } from "./data";
 
 export const BottomMenu = () => {
   return (
-    <nav className="fixed bottom-5 left-1/2 w-full -translate-x-1/2 bg-[#faf9f5] font-smooch-sans text-[16px] md:bottom-0 md:tracking-wider max-[391px]:text-[12px]">
+    <nav className="fixed bottom-10 left-1/2 w-full -translate-x-1/2 bg-[#faf9f5] font-smooch-sans text-[16px] md:bottom-0 md:tracking-wider max-[391px]:text-[12px]">
       <ul className="flex items-center justify-center gap-1 whitespace-nowrap">
         <li>[</li>
         {menuItems.map(({ label, href }, index) => (
